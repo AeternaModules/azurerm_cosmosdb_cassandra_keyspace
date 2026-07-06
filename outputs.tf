78c78fc680064fc0c08088993bcee044c802ce9c
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "cosmosdb_cassandra_keyspaces" {
   description = "All cosmosdb_cassandra_keyspace resources"
   value       = azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces
 }
+output "cosmosdb_cassandra_keyspaces_account_name" {
+  description = "List of account_name values across all cosmosdb_cassandra_keyspaces"
+  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.account_name]
+}
+output "cosmosdb_cassandra_keyspaces_autoscale_settings" {
+  description = "List of autoscale_settings values across all cosmosdb_cassandra_keyspaces"
+  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.autoscale_settings]
+}
+output "cosmosdb_cassandra_keyspaces_name" {
+  description = "List of name values across all cosmosdb_cassandra_keyspaces"
+  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.name]
+}
+output "cosmosdb_cassandra_keyspaces_resource_group_name" {
+  description = "List of resource_group_name values across all cosmosdb_cassandra_keyspaces"
+  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.resource_group_name]
+}
+output "cosmosdb_cassandra_keyspaces_throughput" {
+  description = "List of throughput values across all cosmosdb_cassandra_keyspaces"
+  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.throughput]
+}
 
