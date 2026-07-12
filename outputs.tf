fc434b68c2b504e37ac9e03f305e9d712bf35595
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "cosmosdb_cassandra_keyspaces" {
-  description = "All cosmosdb_cassandra_keyspace resources"
-  value       = azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces
-}
 output "cosmosdb_cassandra_keyspaces_account_name" {
-  description = "List of account_name values across all cosmosdb_cassandra_keyspaces"
-  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.account_name]
+  description = "Map of account_name values across all cosmosdb_cassandra_keyspaces, keyed the same as var.cosmosdb_cassandra_keyspaces"
+  value       = { for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : k => v.account_name }
 }
 output "cosmosdb_cassandra_keyspaces_autoscale_settings" {
-  description = "List of autoscale_settings values across all cosmosdb_cassandra_keyspaces"
-  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.autoscale_settings]
+  description = "Map of autoscale_settings values across all cosmosdb_cassandra_keyspaces, keyed the same as var.cosmosdb_cassandra_keyspaces"
+  value       = { for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : k => v.autoscale_settings }
 }
 output "cosmosdb_cassandra_keyspaces_name" {
-  description = "List of name values across all cosmosdb_cassandra_keyspaces"
-  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.name]
+  description = "Map of name values across all cosmosdb_cassandra_keyspaces, keyed the same as var.cosmosdb_cassandra_keyspaces"
+  value       = { for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : k => v.name }
 }
 output "cosmosdb_cassandra_keyspaces_resource_group_name" {
-  description = "List of resource_group_name values across all cosmosdb_cassandra_keyspaces"
-  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all cosmosdb_cassandra_keyspaces, keyed the same as var.cosmosdb_cassandra_keyspaces"
+  value       = { for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : k => v.resource_group_name }
 }
 output "cosmosdb_cassandra_keyspaces_throughput" {
-  description = "List of throughput values across all cosmosdb_cassandra_keyspaces"
-  value       = [for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : v.throughput]
+  description = "Map of throughput values across all cosmosdb_cassandra_keyspaces, keyed the same as var.cosmosdb_cassandra_keyspaces"
+  value       = { for k, v in azurerm_cosmosdb_cassandra_keyspace.cosmosdb_cassandra_keyspaces : k => v.throughput }
 }
 
